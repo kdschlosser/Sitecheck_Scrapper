932d78ccc954e979256afc4bfa73f22d93209c07
Dan Edens <Dan.Edens@Geo-instruments.com>
2020-01-08 15:55:02 -0600

    Adds text.py

--- _main_.py
+++ _main_.py
@@ -1,23 +1,39 @@
+#Daily Sitecheck Web Scrapper V. 4.0.0
+import sys
 import asyncio
 from pyppeteer import launch
+sys.path.append('/env/')
+from env import sites,text
 
 
-async def main():
-    browser = await launch(headless=False)
-    page = await browser.newPage()
-    await page.goto('http://example.com')
-    await page.screenshot({'path': 'example.png'})
 
-    dimensions = await page.evaluate('''() => {
-        return {
-            width: document.documentElement.clientWidth,
-            height: document.documentElement.clientHeight,
-            deviceScaleFactor: window.devicePixelRatio,
-        }
-    }''')
 
-    print(dimensions)
-    # >>> {'width': 800, 'height': 600, 'deviceScaleFactor': 1}
-    await browser.close()
 
-asyncio.get_event_loop().run_until_complete(main())
+
+
+
+
+
+
+
+
+# async def main():
+    
+    # browser = await launch(headless=False)
+    # page = await browser.newPage()
+#     await page.goto('http://example.com')
+#     await page.screenshot({'path': 'example.png'})
+
+#     dimensions = await page.evaluate('''() => {
+#         return {
+#             width: document.documentElement.clientWidth,
+#             height: document.documentElement.clientHeight,
+#             deviceScaleFactor: window.devicePixelRatio,
+#         }
+#     }''')
+
+#     print(dimensions)
+#     # >>> {'width': 800, 'height': 600, 'deviceScaleFactor': 1}
+#     await browser.close()
+
+# asyncio.get_event_loop().run_until_complete(main())
---
+++ test.py
@@ -0,0 +1,4 @@
+import keyring
+
+x = keyring.set_password("github", "'Dan.Edens@Geo-instruments.com'", "geoint31Z")
+print (x)
