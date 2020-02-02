975ba437d75a84a3831945fe1f367ab4c85ae363
Dan Edens <Dan.Edens@Geo-instruments.com>
2020-02-02 11:39:37 -0600

    Ignores data dir

---
+++ bin/optimizer.py
@@ -0,0 +1,24 @@
+from __main__ import ROOT_DIR
+
+data = ROOT_DIR + "/env/data/"
+
+class Memory:
+    def __init__(self, project, view, sensor_id):
+        self.project = project
+        self.view = view
+        self.sensor_id = sensor_id
+
+    def stage_out(self):
+        # write out key with project,view, and ID for future runs
+        # append y,z to array x
+        sensor_ids += self.sensor_id
+        pass
+
+    def write_out(self):
+        # On run end, rewrite line in storage file
+        print(staged_ID, data+self.project+'.txt')
+        pass
+
+class Recall:
+    def __init__(self):
+        self.
