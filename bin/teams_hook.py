ca4e4931167051cff3f040e30df3bdf8ef6fb88e
Dan Edens <Dan.Edens@Geo-instruments.com>
2020-02-05 13:09:42 -0600

    Finishes changes to results and spacing reformat.

--- bin/teams_hook.py
+++ bin/teams_hook.py
@@ -4,9 +4,9 @@ Documentation for Team's card incoming Webhooks:
 https://docs.microsoft.com/en-us/micrsoftteams/platform/webhooks-and-connectors/what-are-webhooks-and-connectors
 
 """
-import asyncio
 import json
 
+import asyncio
 import requests
 
 from env import creds, text
@@ -27,33 +27,38 @@ def top_secret(channel):
     elif channel == 'West-project':
         return creds.westcoasthook
     else:
-        print ( text.no_channel )
+        print(text.no_channel)
         return creds.testhook
 
 
 class Send_Hook:
-    def __init__(self, channel, temp_project, tempfile_path):
+    """
+    Send json data through a webhook to the Team's channel
+    """
+
+    def __init__(self, channel, temp_project, temp_file_path):
         """
                   Args:
-                      channel (str): Which team to send card to. Currently 1 option
-                      tempfile_path (str): 'Path to json being Posted'
+                      channel (str): Which channel(team) to send card to
+                      temp_project (str): Project name and filename
+                      temp_file_path (str): Path to json being Posted
                   Returns:
                       (str): Post error message
                   """
         # converts channel name to url from creds file
-        self.channel = top_secret ( channel )
+        self.channel = top_secret(channel)
         self.project = temp_project
         self.file = file_path
-        with open ( self.file + '_card.json', 'r' ) as f:
-            file = f.read ()
-        self.finished_card = json.loads ( file )
-        self.draft_message ()
+        with open(self.file + '_card.json') as f:
+            file = f.read()
+        self.finished_card = json.loads(file)
+        self.draft_message()
 
     def draft_message(self):
-        # prompt user to review card.
+        """Prompt user to review card."""
         # TODO: build Interactive module
         # TODO: find way to display preview
-        print ( self )
+        print(self)
         asyncio.get_event_loop().run_until_complete(self.send_message())
 
     async def send_message(self):
@@ -63,12 +68,11 @@ class Send_Hook:
                 headers={'Content-Type': 'application/json'}
         )
         if response.status_code != 200:
-            print(ValueError(
-                'Request to Teams returned an error %s, the response is:\n%s' % (response.status_code, response.text)))
-            # raise ValueError (
-            #     'Request to Teams returned an error %s, the response is:\n%s'
-            #     % (response.status_code, response.text)
-            # )
+            result = ValueError(
+                    'Request to Teams returned an error %s, the response is:\n%s' % (
+                            response.status_code, response.text))
+            print(result)
+            return result
 
 
 if __name__ == '__main__':
