39b50a1949babf42091002c5794e61c46575beea
Dan Edens <Dan.Edens@Geo-instruments.com>
2020-02-14 13:04:04 -0600

    Adds example creds file

---
+++ env/creds.py
@@ -0,0 +1,19 @@
+"""
+    Example Creds File
+    This File is ignored from Git index and exists to prevent
+    your personal information from being mistakenly made public
+"""
+user = "dan.edens"
+chromeexe = "C =\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+testhook = "https://webhook.site/8a749da3-5b61-4060-b64f-22e3ae303f62"
+westcoasthook = "https://prod-111.westus.logic.azure.com:443/workflows/*"
+ampuser = "Dan Edens"
+amppass = "*****"
+ampadminuser = "geouser"
+ampadminpass = "G********!"
+ampadminuser2 = "Dan Edens"
+ampadminpass2 = "****"
+qvuser = "admin"
+qvpass = "********"
+checkuser_user = "test_account_name"
+checkuser_pass = "test_password"
